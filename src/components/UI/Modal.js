7d170b020368c1791708458d825d919b4ea6b8c8
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2023-03-01 12:07:56 +0400

    Fixed Modal window with portal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,25 +1,47 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import styles from './Modal.module.css';
 import Card from './Card';
 import Button from './Button';
 
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onConfirm}></div>;
+};
+
+const ModalWindow = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button type="button" onClick={props.onConfirm}>
+          Ok
+        </Button>
+      </footer>
+    </Card>
+  );
+};
+
 const Modal = (props) => {
   return (
     <>
-      <div className={styles.backdrop} onClick={props.onConfirm}></div>;
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button type="button" onClick={props.onConfirm}>
-            Ok
-          </Button>
-        </footer>
-      </Card>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById('backdrop-root')
+      )}
+
+      {ReactDOM.createPortal(
+        <ModalWindow
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById('modal-root')
+      )}
     </>
   );
 };
