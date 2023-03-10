c0fabbbb940a63c1a557e740cbfaee64179a89a1
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2023-03-10 17:02:29 +0400

    Added image to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import styles from './Header.module.css';
 import Navigation from './Navigation';
+import goalImage from '../../assets/goal-ladder-4.png';
 
 const Header = (props) => {
   return (
-    <header className={styles.header}>
-      <h1>Goals Tracker</h1>
-      <Navigation onLogout={props.onLogout} />
-    </header>
+    <React.Fragment>
+      <header className={styles.header}>
+        <h1>Goals Tracker</h1>
+        <Navigation onLogout={props.onLogout} />
+      </header>
+      <div className={styles['main-image']}>
+        <img src={goalImage} alt="Climbing to achieve a goal" />
+      </div>
+    </React.Fragment>
   );
 };
 
