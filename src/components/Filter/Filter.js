b6fae010a5ff102911874ebc6df9208478918adc
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2023-03-25 22:26:14 +0400

    Added Goal list filters

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,30 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../UI/Button';
 import styles from './Filter.module.css';
 
-const Filter = () => {
+const Filter = (props) => {
+  const [filter, setFilter] = useState('');
+
+  const filterChangeHandler = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const filterHandler = (event) => {
+    event.preventDefault();
+    props.onFilter(filter);
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={filterHandler}>
       <Button>Filter</Button>
       <div className={styles['form-inputs']}>
         <div className={styles['form-input']}>
-          <input type="radio" id="all" name="filter" />
+          <input
+            type="radio"
+            id="all"
+            name="filter"
+            value="All"
+            onChange={filterChangeHandler}
+          />
           <label htmlFor="all">All</label>
         </div>
         <div className={styles['form-input']}>
-          <input type="radio" id="personal" name="filter" />
+          <input
+            type="radio"
+            id="personal"
+            name="filter"
+            value="Personal"
+            onChange={filterChangeHandler}
+          />
           <label htmlFor="personal">Personal</label>
         </div>
         <div className={styles['form-input']}>
-          <input type="radio" id="work" name="filter" />
+          <input
+            type="radio"
+            id="work"
+            name="filter"
+            value="Work"
+            onChange={filterChangeHandler}
+          />
           <label htmlFor="work">Work</label>
         </div>
         <div className={styles['form-input']}>
-          <input type="radio" id="education" name="filter" />
+          <input
+            type="radio"
+            id="education"
+            name="filter"
+            value="Education"
+            onChange={filterChangeHandler}
+          />
           <label htmlFor="education">Education</label>
         </div>
         <div className={styles['form-input']}>
-          <input type="radio" id="health" name="filter" />
+          <input
+            type="radio"
+            id="health"
+            name="filter"
+            value="Health"
+            onChange={filterChangeHandler}
+          />
           <label htmlFor="health">Health</label>
         </div>
       </div>
--- src/components/GoalsList/GoalsList.js
+++ src/components/GoalsList/GoalsList.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Filter from '../Filter/Filter';
 import Card from '../UI/Card';
 import Goal from './Goal';
 import styles from './GoalList.module.css';
 
 const GoalsList = (props) => {
+  const [filterSelection, setFilterSelection] = useState('');
+
   const clickedGoal = (title, id) => {
     props.onCompleteGoal({ competedGoal: title, completedId: id });
   };
 
+  const filterHandler = (filterSelection) => {
+    setFilterSelection(filterSelection);
+  };
+
+  const filteredList = props.goals.filter((goal) => {
+    if (filterSelection === '' || filterSelection === 'All') {
+      return true;
+    } else {
+      return goal.category === filterSelection;
+    }
+  });
+
   return (
     <Card>
-      <Filter />
+      <Filter onFilter={filterHandler} />
       <ul className={styles['goal-list']}>
-        {props.goals.map((goal) => (
+        {filteredList.map((goal) => (
           <Goal
             title={goal.title}
             time={goal.time}
